66836aec05d5759943caaddd201d96582844b753
Hassan <96440779+hassan6846@users.noreply.github.com>
2025-02-26 17:43:05 +0500

    added pages

---
+++ app/screens/AllNotes/AllNotes.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AllNotes = () => {
+  return (
+    <View>
+      <Text>AllNotes</Text>
+    </View>
+  )
+}
+
+export default AllNotes
+
+const styles = StyleSheet.create({})
